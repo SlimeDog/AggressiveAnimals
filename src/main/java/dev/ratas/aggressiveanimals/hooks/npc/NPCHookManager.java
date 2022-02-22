4596697f45be8544a8961a359d999235377c7964
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-02-22 20:46:01 +0000

    Fixed typo in NPC hook constant name

--- src/main/java/dev/ratas/aggressiveanimals/hooks/npc/NPCHookManager.java
+++ src/main/java/dev/ratas/aggressiveanimals/hooks/npc/NPCHookManager.java
@@ -5,11 +5,11 @@ public class NPCHookManager extends OneOfDelegateNPCHook {
     private static final NPCHook CITIZENS_HOOK = new MetadataNPCHook("Citizens", "NPC");
     private static final NPCHook SHOPKEEPER_HOOK = new MetadataNPCHook("Shopkeeper", "shopkeeper");
     private static final NPCHook INFERNAL_MOBS_HOOK = new MetadataNPCHook("InfernalMobs", "infernalMetadata");
-    private static final NPCHook ELITE__MOBS_HOOK = new OneOfDelegateNPCHook("EliteMobs",
+    private static final NPCHook ELITE_MOBS_HOOK = new OneOfDelegateNPCHook("EliteMobs",
             new MetadataNPCHook("Elitemobs_NPC", "Elitemobs_NPC"), new MetadataNPCHook("Elitemob", "Elitemob"));
 
     public NPCHookManager() {
-        super(COMBINED_HOOKS, CITIZENS_HOOK, SHOPKEEPER_HOOK, INFERNAL_MOBS_HOOK, ELITE__MOBS_HOOK);
+        super(COMBINED_HOOKS, CITIZENS_HOOK, SHOPKEEPER_HOOK, INFERNAL_MOBS_HOOK, ELITE_MOBS_HOOK);
     }
 
 }
