d3e3a7afb18ea04cd9284946eccacbff0cf308ed
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-02-22 23:22:46 +0000

    Properly defining (in code) message for issues during reload

--- src/main/java/dev/ratas/aggressiveanimals/config/messaging/Messages.java
+++ src/main/java/dev/ratas/aggressiveanimals/config/messaging/Messages.java
@@ -5,14 +5,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Messages extends MessagesBase {
     private final Message reloadMessage;
+    private final Message reloadFailMessage;
 
     public Messages(JavaPlugin plugin) throws InvalidConfigurationException {
         super(plugin);
         this.reloadMessage = new Message(this, "reloaded-config", "Plugin was successfully reloaded");
+        this.reloadFailMessage = new Message(this, "problem-reloading-config",
+                "There was an issue while reloading the config - check the console log");
     }
 
     public Message getReloadMessage() {
         return reloadMessage;
+    }
+
+    public Message getReloadFailedMessage() {
+        return reloadFailMessage;
     }
 
 }
