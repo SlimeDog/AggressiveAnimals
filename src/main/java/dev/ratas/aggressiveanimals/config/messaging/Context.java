c7ba897df25b864506cdd9fe57c1e883fa149bda
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-02-23 21:35:36 +0000

    Revamping how message context works. Example context for player target

--- src/main/java/dev/ratas/aggressiveanimals/config/messaging/Context.java
+++ src/main/java/dev/ratas/aggressiveanimals/config/messaging/Context.java
@@ -2,33 +2,33 @@ package dev.ratas.aggressiveanimals.config.messaging;
 
 import org.bukkit.entity.Player;
 
-public class Context {
-    private static final String TARGET_PLACEHOLDER = "{target}";
-    private final String placeholder;
-    private final Player namedTarget;
+public interface Context {
+    String fill(String msg);
 
-    private Context(String placeholder, Player namedTarget) {
-        this.placeholder = placeholder;
-        this.namedTarget = namedTarget;
-    }
+    public static class PlayerTargetContext implements Context {
+        private static final String TARGET_PLACEHOLDER = "%target%";
+        private final String placeholder;
+        private final Player namedTarget;
 
-    public String fill(String msg) {
-        if (namedTarget != null) {
-            msg = replaceWith(msg, placeholder, namedTarget.getName());
+        public PlayerTargetContext(String placeholder, Player namedTarget) {
+            this.placeholder = placeholder;
+            this.namedTarget = namedTarget;
         }
-        return msg;
-    }
 
-    private String replaceWith(String msg, String target, String replacement) {
-        return msg.replace(target, replacement);
-    }
+        public String fill(String msg) {
+            if (namedTarget != null) {
+                msg = replaceWith(msg, placeholder, namedTarget.getName());
+            }
+            return msg;
+        }
 
-    public static Context playerTarget(String placeholder, Player player) {
-        return new Context(placeholder, player);
-    }
+        private String replaceWith(String msg, String target, String replacement) {
+            return msg.replace(target, replacement);
+        }
 
-    public static Context defaultPlayerTarget(Player player) {
-        return playerTarget(TARGET_PLACEHOLDER, player);
+        public static Context defaultPlaceholder(Player player) {
+            return new PlayerTargetContext(TARGET_PLACEHOLDER, player);
+        }
     }
 
 }
