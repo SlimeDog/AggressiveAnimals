dccbeaf78c444900eddc0853ea2e9fd29585d269
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-03-01 07:02:54 +0000

    Now using the new simplified ways of creating message factories to improve readability and remove code clutter

--- src/main/java/dev/ratas/aggressiveanimals/config/messaging/Messages.java
+++ src/main/java/dev/ratas/aggressiveanimals/config/messaging/Messages.java
@@ -6,16 +6,11 @@ import dev.ratas.aggressiveanimals.aggressive.settings.MobType;
 import dev.ratas.slimedogcore.api.SlimeDogPlugin;
 import dev.ratas.slimedogcore.api.messaging.SDCMessage;
 import dev.ratas.slimedogcore.api.messaging.context.SDCVoidContext;
-import dev.ratas.slimedogcore.api.messaging.delivery.MessageTarget;
 import dev.ratas.slimedogcore.api.messaging.factory.SDCDoubleContextMessageFactory;
 import dev.ratas.slimedogcore.api.messaging.factory.SDCVoidContextMessageFactory;
 import dev.ratas.slimedogcore.impl.messaging.MessagesBase;
 import dev.ratas.slimedogcore.impl.messaging.context.VoidContext;
-import dev.ratas.slimedogcore.impl.messaging.context.factory.SingleContextFactory;
-import dev.ratas.slimedogcore.impl.messaging.context.factory.VoidContextFactory;
-import dev.ratas.slimedogcore.impl.messaging.context.factory.delegating.DelegatingDoubleContextFactory;
-import dev.ratas.slimedogcore.impl.messaging.factory.DoubleContextMessageFactory;
-import dev.ratas.slimedogcore.impl.messaging.factory.VoidContextMessageFactory;
+import dev.ratas.slimedogcore.impl.messaging.factory.MsgUtil;
 
 public class Messages extends MessagesBase {
     private static final String FILE_NAME = "messages.yml";
@@ -32,26 +27,16 @@ public class Messages extends MessagesBase {
     }
 
     private void loadMessages() {
-        this.reloadMessage = new VoidContextMessageFactory(VoidContextFactory.INSTANCE,
-                getRawMessage("reloaded-config", "Plugin was successfully reloaded"), MessageTarget.TEXT);
-        this.reloadFailMessage = new VoidContextMessageFactory(VoidContextFactory.INSTANCE,
-                getRawMessage("problem-reloading-config",
-                        "There was an issue while reloading the config - check the console log"),
-                MessageTarget.TEXT);
-        this.listHeaderMessage = new VoidContextMessageFactory(VoidContextFactory.INSTANCE,
-                getRawMessage("list-header", "&8Configured mobs"), MessageTarget.TEXT);
-        this.listItemMessage = new DoubleContextMessageFactory<>(
-                new DelegatingDoubleContextFactory<>(new SingleContextFactory<>("%mob-type%", t -> t.name()),
-                        new SingleContextFactory<>("%status%", b -> {
-                            SDCMessage<SDCVoidContext> m = (b ? enabledMessage : disabledMessage)
-                                    .getMessage(VoidContext.INSTANCE);
-                            return m.getRaw();
-                        })),
-                getRawMessage("list-format", "&6%mob-type% &f- %status%"), MessageTarget.TEXT);
-        this.enabledMessage = new VoidContextMessageFactory(VoidContextFactory.INSTANCE,
-                getRawMessage("enabled", "enabled"), MessageTarget.TEXT);
-        this.disabledMessage = new VoidContextMessageFactory(VoidContextFactory.INSTANCE,
-                getRawMessage("disabled", "disabled"), MessageTarget.TEXT);
+        this.reloadMessage = MsgUtil.voidContext(getRawMessage("reloaded-config", "Plugin was successfully reloaded"));
+        this.reloadFailMessage = MsgUtil.voidContext(getRawMessage("problem-reloading-config",
+                "There was an issue while reloading the config - check the console log"));
+        this.listHeaderMessage = MsgUtil.voidContext(getRawMessage("list-header", "&8Configured mobs"));
+        this.listItemMessage = MsgUtil.doubleContext("%mob-type%", t -> t.name(), "%status%", b -> {
+            SDCMessage<SDCVoidContext> m = (b ? enabledMessage : disabledMessage).getMessage(VoidContext.INSTANCE);
+            return m.getFilled();
+        }, getRawMessage("list-format", "&6%mob-type% &f- %status%"));
+        this.enabledMessage = MsgUtil.voidContext(getRawMessage("enabled", "enabled"));
+        this.disabledMessage = MsgUtil.voidContext(getRawMessage("disabled", "disabled"));
     }
 
     public SDCVoidContextMessageFactory getReloadMessage() {
