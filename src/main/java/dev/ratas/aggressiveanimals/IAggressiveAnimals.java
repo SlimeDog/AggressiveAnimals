fd5bee0061fd97b6891ed9481a0a8b3b991ca956
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-03-02 07:35:28 +0000

    Added asPlugin method to interface

--- src/main/java/dev/ratas/aggressiveanimals/IAggressiveAnimals.java
+++ src/main/java/dev/ratas/aggressiveanimals/IAggressiveAnimals.java
@@ -1,9 +1,14 @@
 package dev.ratas.aggressiveanimals;
 
 import dev.ratas.slimedogcore.api.SlimeDogPlugin;
+import dev.ratas.slimedogcore.impl.SlimeDogCore;
 
 public interface IAggressiveAnimals extends SlimeDogPlugin {
 
     void debug(String msg);
+
+    default SlimeDogCore asPlugin() {
+        return (SlimeDogCore) this;
+    }
 
 }
