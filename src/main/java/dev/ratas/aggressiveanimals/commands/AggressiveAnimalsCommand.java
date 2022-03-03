1e781beb9a6a7b14f95c52836eb13b766c5ecd1a
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-03-03 21:56:41 +0000

    Readded usage message on 0-argument parent command (oversite on SDC)

--- src/main/java/dev/ratas/aggressiveanimals/commands/AggressiveAnimalsCommand.java
+++ src/main/java/dev/ratas/aggressiveanimals/commands/AggressiveAnimalsCommand.java
@@ -1,11 +1,14 @@
 package dev.ratas.aggressiveanimals.commands;
 
+import java.util.List;
+
 import dev.ratas.aggressiveanimals.IAggressiveAnimals;
 import dev.ratas.aggressiveanimals.commands.sub.HelpSub;
 import dev.ratas.aggressiveanimals.commands.sub.InfoSub;
 import dev.ratas.aggressiveanimals.commands.sub.ListSub;
 import dev.ratas.aggressiveanimals.commands.sub.ReloadSub;
 import dev.ratas.aggressiveanimals.config.messaging.Messages;
+import dev.ratas.slimedogcore.api.messaging.recipient.SDCRecipient;
 import dev.ratas.slimedogcore.impl.commands.BukkitFacingParentCommand;
 
 public class AggressiveAnimalsCommand extends BukkitFacingParentCommand {
@@ -15,6 +18,15 @@ public class AggressiveAnimalsCommand extends BukkitFacingParentCommand {
         addSubCommand(new ListSub(plugin.getAggressivityManager().getMobTypeManager(), messages));
         addSubCommand(new InfoSub(plugin.getAggressivityManager(), messages));
         addSubCommand(new HelpSub(this));
+    }
+
+    @Override
+    public boolean onCommand(SDCRecipient sender, String[] args, List<String> opts) {
+        if (args.length == 0) {
+            sender.sendRawMessage(getUsage(sender));
+            return true;
+        }
+        return super.onCommand(sender, args, opts);
     }
 
 }
