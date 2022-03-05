38d8f7642a9cc8a4b9a025e12d982d8223403303
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-03-05 07:41:43 +0000

    Fixed tameability check for aggressiveness

--- src/main/java/dev/ratas/aggressiveanimals/aggressive/settings/type/MobMiscSettings.java
+++ src/main/java/dev/ratas/aggressiveanimals/aggressive/settings/type/MobMiscSettings.java
@@ -1,23 +1,35 @@
 package dev.ratas.aggressiveanimals.aggressive.settings.type;
 
-import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Tameable;
 
-import dev.ratas.aggressiveanimals.aggressive.settings.MobType;
 import dev.ratas.aggressiveanimals.hooks.npc.NPCHookManager;
 
 public record MobMiscSettings(boolean includeNpcs, boolean targetAsNamedOnly, boolean includeTamed) {
 
-    public boolean shouldBeAggressive(NPCHookManager npcHooks, LivingEntity mob, LivingEntity target) {
+    public boolean shouldBeAggressive(NPCHookManager npcHooks, Mob mob, Player target) {
         if (!includeNpcs && npcHooks.isNPC(target)) {
             return false;
         }
         if (targetAsNamedOnly && mob.getCustomName() == null) {
             return false;
         }
-        if (!includeTamed && MobType.fromBukkit(mob.getType()).isTameable()) {
+        if (!shouldBeAggressiveRegardingTamability(mob)) {
             return false;
         }
         return true;
+    }
+
+    private boolean shouldBeAggressiveRegardingTamability(Mob mob) {
+        if (!(mob instanceof Tameable tameable)) {
+            return true;
+        }
+        if (includeTamed) {
+            return true; // be aggressive regardless
+        } else { // aggressive only if mob is NOT tamed
+            return !tameable.isTamed();
+        }
     }
 
 }
